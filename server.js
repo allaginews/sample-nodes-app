01baf5ae67b99aa37370ba6e7b9b0b4aa0ac4e04
allaginews <siankit11@gmail.com>
2020-05-30 20:45:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 const express = require('express');const app = express()
-app.get('/', (req, res) => {    res.send('Hello world from a Node.js app!')})
+app.get('/', (req, res) => {    res.send('Hello world from a Node.js app github1!')})
 app.listen(3000, () => {    console.log('Server is up on 3000')})
